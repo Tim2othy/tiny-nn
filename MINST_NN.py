0497eec4e09f734b3c71f52dbbd7fec1bbac2424
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2024-08-09 22:28:54 +0200

    started all in one

--- MINST_NN.py
+++ MINST_NN.py
@@ -33,7 +33,7 @@ net.add(ActivationLayer(tanh, tanh_prime))
 
 # Set loss and train the network
 net.use(mse, mse_prime)
-net.fit(x_train[:20], y_train[:20], epochs=500, learning_rate=0.07)
+net.fit(x_train[:20], y_train[:20], epochs=500, learning_rate=0.04)
 
 # Test the network on a few samples
 out = net.predict(x_test[:3])

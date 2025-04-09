c4afe2a8dc06732aa1dbb415c82c920eb7b1e49a
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2025-04-09 10:56:23 +0200

    split mnist loader and visualizer into two files

--- mnist_loader.py
+++ mnist_loader.py
@@ -1,17 +1,14 @@
-#
-# This is a sample Notebook to demonstrate how to read "MNIST Dataset"
-#
-import random
+"""This reads in the "MNIST Dataset" and visualizes it"""
+
 import struct
 from array import array
 
-import matplotlib.pyplot as plt
-import numpy as np  # linear algebra
+import numpy as np
 
 
-#
 # MNIST Data Loader Class
-#
+
+
 class MnistDataloader(object):
     def __init__(
         self,
@@ -60,64 +57,3 @@ class MnistDataloader(object):
             self.test_images_filepath, self.test_labels_filepath
         )
         return (x_train, y_train), (x_test, y_test)
-
-
-### break
-
-
-# Set file paths based on added MNIST Datasets
-training_images_filepath = "input/train-images-idx3-ubyte"
-training_labels_filepath = "input/train-labels-idx1-ubyte"
-test_images_filepath = "input/t10k-images-idx3-ubyte"
-test_labels_filepath = "input/t10k-labels-idx1-ubyte"
-
-
-#
-# Helper function to show a list of images with their relating titles
-#
-def show_images(images, title_texts):
-    cols = 5
-    rows = int(len(images) / cols) + 1
-    plt.figure(figsize=(30, 20))
-    index = 1
-    for x in zip(images, title_texts):
-        image = x[0]
-        title_text = x[1]
-        plt.subplot(rows, cols, index)
-        plt.imshow(image, cmap="gray")
-        if title_text != "":
-            plt.title(title_text, fontsize=15)
-        index += 1
-
-
-#
-# Load MINST dataset
-#
-mnist_dataloader = MnistDataloader(
-    training_images_filepath,
-    training_labels_filepath,
-    test_images_filepath,
-    test_labels_filepath,
-)
-(x_train, y_train), (x_test, y_test) = mnist_dataloader.load_data()
-
-#
-# Show some random training and test images
-#
-images_2_show = []
-titles_2_show = []
-for i in range(0, 10):
-    r = random.randint(1, 60000)
-    images_2_show.append(x_train[r])
-    titles_2_show.append("training image [" + str(r) + "] = " + str(y_train[r]))
-
-for i in range(0, 5):
-    r = random.randint(1, 10000)
-    images_2_show.append(x_test[r])
-    titles_2_show.append("test image [" + str(r) + "] = " + str(y_test[r]))
-
-show_images(images_2_show, titles_2_show)
-
-plt.show()
-
-print("end")

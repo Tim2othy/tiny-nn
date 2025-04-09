ca9cf1fd58713f806fd4bc59660f96a4765996f3
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2025-04-09 14:36:40 +0200

    update xor net

--- XOR.py
+++ XOR.py
@@ -1,8 +1,7 @@
 import numpy as np
 
 from activation_Layer import ActivationLayer
-from activations import tanh, tanh_prime
-from fc_layer import FCLayer
+from layers import FCLayer, tanh, tanh_prime
 from losses import mse, mse_prime
 from network import Network
 
@@ -11,7 +10,7 @@ x_train = np.array([[[0, 0]], [[0, 1]], [[1, 0]], [[1, 1]]])
 y_train = np.array([[[0]], [[1]], [[1]], [[0]]])
 
 # network
-net = Network()
+net = Network(mse, mse_prime)
 net.add(FCLayer(2, 3))
 net.add(ActivationLayer(tanh, tanh_prime))
 net.add(FCLayer(3, 1))
@@ -19,7 +18,6 @@ net.add(ActivationLayer(tanh, tanh_prime))
 
 
 # train
-net.use(mse, mse_prime)
 net.fit(x_train, y_train, epochs=1000, learning_rate=0.1)
 
 

91765f1959aac577efb831f56a8fb7f7597fd705
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2025-04-10 17:58:51 +0200

    rename data and other small changes to even more minimal

--- even_more_minimal.py
+++ even_more_minimal.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from load_mnist import test_data, training_data
+from load_mnist import x_test, x_train, y_test, y_train
 
 
 def mse(y_true, y_pred):
@@ -16,7 +16,7 @@ def softmax(x):
     return exp_values / np.sum(exp_values, axis=1, keepdims=True)
 
 
-def fc_bp(bias, weights, input, output_error):
+def backprop_fc(bias, weights, input, output_error):
     input_error = np.dot(output_error, weights.T)
 
     weights -= 0.04 * np.dot(input.T, output_error)
@@ -35,7 +35,7 @@ b3 = np.random.rand(1, 10) - 0.5
 train_loss = 0
 for i in range(60000):
     """forward propagation"""
-    pixels = training_data[0][i]
+    pixels = x_train[i]
 
     output1 = np.dot(pixels, w1) + b1
     output2 = np.dot(output1, w2) + b2
@@ -43,27 +43,27 @@ for i in range(60000):
     prediction = softmax(output3)
 
     """backward propagation"""
-    error = mse_prime(training_data[1][i], prediction)
-    error = fc_bp(b3, w3, output2, error)
-    error = fc_bp(b2, w2, output1, error)
-    error = fc_bp(b1, w1, pixels, error)
+    error = mse_prime(y_train[i], prediction)
+    error = backprop_fc(b3, w3, output2, error)
+    error = backprop_fc(b2, w2, output1, error)
+    error = backprop_fc(b1, w1, pixels, error)
 
-    train_loss += mse(training_data[1][i], prediction)
+    train_loss += mse(y_train[i], prediction)
     if (i + 1) % 12000 == 0:
-        print(f"At {i + 1}/{60000}   the error is {train_loss / 12000:.3f}")
+        print(f"At {i + 1}/{60000} the error is {train_loss / 12000:.3f}")
         train_loss = 0
 
 
 test_loss = 0
 for i in range(10000):
     """forward propagation"""
-    pixels = test_data[0][i]
+    pixels = x_test[i]
 
     output = np.dot(pixels, w1) + b1
     output = np.dot(output, w2) + b2
     output = np.dot(output, w3) + b3
     prediction = softmax(output)
 
-    test_loss += mse(test_data[1][i], prediction)
+    test_loss += mse(y_test[i], prediction)
 
 print(f"Test loss: {test_loss / 10000:.3f}")

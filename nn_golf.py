11b1978410b277d2e046215448ac60f37f2c14e5
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2025-10-28 10:25:09 +0100

    shorter variable names

--- nn_golf.py
+++ nn_golf.py
@@ -9,16 +9,16 @@ def b(b,w,i,u):
     b-=0.04*sum(u)
     return g
 def f(a,b):return random.rand(a,b)-0.5
-w1,b1,w2,b2,w3,b3,t,e=(f(784,100),f(1,100),f(100,50),f(1,50),f(50,10),f(1,10),10000,0)
+w,c,v,d,x,j,t,e=(f(784,100),f(1,100),f(100,50),f(1,50),f(50,10),f(1,10),10000,0)
 for i in range(t*6):
-    o1=dot(xt[i],w1)+b1
-    o2=dot(o1,w2)+b2
-    r=s(dot(o2,w3)+b3)
-    b(b1,w1,xt[i],b(b2,w2,o1,b(b3,w3,o2,(r-yt[i])/r.size)))
+    q=dot(xt[i],w)+c
+    n=dot(q,v)+d
+    r=s(dot(n,x)+j)
+    b(c,w,xt[i],b(d,v,q,b(j,x,n,(r-yt[i])/r.size)))
     e+=mean((yt[i]-r)**2)
     if (i+1)%t==0:
         print(f"At {i+1}/{t*6} the error is {e/t}")
         e=0
 for i in range(t):
-    e+=mean((ys[i]-s(dot(dot(dot(xs[i],w1)+b1,w2)+b2,w3)+b3))**2)
+    e+=mean((ys[i]-s(dot(dot(dot(xs[i],w)+c,v)+d,x)+j))**2)
 print(f"Test loss: {e/t}")

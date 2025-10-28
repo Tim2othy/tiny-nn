f30fd564eb04fee6aca29fdfc640985dd5f7bc88
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2025-10-28 10:13:16 +0100

    import numpy differently in nn_golf

--- nn_golf.py
+++ nn_golf.py
@@ -1,24 +1,24 @@
-import numpy as n
+from numpy import dot,exp,max,mean,random,sum
 from load_mnist import xs,xt,ys,yt
 def s(x):
-    h=n.exp(x-n.max(x))
-    return h/n.sum(h)
+    h=exp(x-max(x))
+    return h/sum(h)
 def b(b,w,i,u):
-    g=n.dot(u,w.T)
-    w-=0.04*n.dot(i.T,u)
-    b-=0.04*n.sum(u)
+    g=dot(u,w.T)
+    w-=0.04*dot(i.T,u)
+    b-=0.04*sum(u)
     return g
-def f(a,b):return n.random.rand(a,b)-0.5
+def f(a,b):return random.rand(a,b)-0.5
 w1,b1,w2,b2,w3,b3,t,e=(f(784,100),f(1,100),f(100,50),f(1,50),f(50,10),f(1,10),10000,0)
 for i in range(t*6):
-    o1=n.dot(xt[i],w1)+b1
-    o2=n.dot(o1,w2)+b2
-    r=s(n.dot(o2,w3)+b3)
+    o1=dot(xt[i],w1)+b1
+    o2=dot(o1,w2)+b2
+    r=s(dot(o2,w3)+b3)
     b(b1,w1,xt[i],b(b2,w2,o1,b(b3,w3,o2,(r-yt[i])/r.size)))
-    e+=n.mean((yt[i]-r)**2)
+    e+=mean((yt[i]-r)**2)
     if (i+1)%t==0:
         print(f"At {i+1}/{t*6} the error is {e/t}")
         e=0
 for i in range(t):
-    e+=n.mean((ys[i]-s(n.dot(n.dot(n.dot(xs[i],w1)+b1,w2)+b2,w3)+b3))**2)
+    e+=mean((ys[i]-s(dot(dot(dot(xs[i],w1)+b1,w2)+b2,w3)+b3))**2)
 print(f"Test loss: {e/t}")

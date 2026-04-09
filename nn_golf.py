d824da4138e97394d2a802517a19fe35b23e7a6a
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2026-04-09 18:55:32 +0200

    use p=dot in golf

--- nn_golf.py
+++ nn_golf.py
@@ -1,16 +1,17 @@
 from numpy import dot,exp,max,mean,random,sum
 from load_mnist import xs,xt,ys,yt
+p=dot
 def b(b,w,i,u):
-    g=dot(u,w.T)
-    w-=0.04*dot(i.T,u)
+    g=p(u,w.T)
+    w-=0.04*p(i.T,u)
     b-=0.04*sum(u)
     return g
 f=lambda a,b:random.rand(a, b)-0.5
 w,c,v,d,x,j,t,e=(f(784,100),f(1,100),f(100,50),f(1,50),f(50,10),f(1,10),10000,0)
 for i in range(t*6):
-    q=dot(xt[i],w)+c
-    n=dot(q,v)+d
-    s=dot(n,x)+j
+    q=p(xt[i],w)+c
+    n=p(q,v)+d
+    s=p(n,x)+j
     r=exp(s-max(s))/sum(exp(s-max(s)))
     b(c,w,xt[i],b(d,v,q,b(j,x,n,(r-yt[i])/r.size)))
     e+=mean((yt[i]-r)**2)
@@ -18,5 +19,5 @@ for i in range(t*6):
         print(f"At {i+1}/{t*6} the error is {e/t}")
         e=0
 for i in range(t):
-    e+=mean((ys[i]-s(dot(dot(dot(xs[i],w)+c,v)+d,x)+j))**2)
+    e+=mean((ys[i]-s(p(p(p(xs[i],w)+c,v)+d,x)+j))**2)
 print(f"Test loss: {e/t}")

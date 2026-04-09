2843ab30e9435e6885d11e44132731bf6a6fff1c
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2026-04-09 19:16:18 +0200

    add relu layers to golf

--- nn_golf.py
+++ nn_golf.py
@@ -1,6 +1,8 @@
-from numpy import dot,exp,max,random,sum,argmax,log
+from numpy import dot,exp,max,random,sum,maximum,argmax,log
 from load_mnist import xs,xt,ys,yt
 p=dot
+o=lambda x: maximum(0,x)
+k=lambda x: (x>0)
 def b(b,w,i,u):
     g=p(u,w.T)
     w-=0.04*p(i.T,u)
@@ -9,11 +11,11 @@ def b(b,w,i,u):
 f=lambda a,b:random.rand(a, b)-0.5
 w,c,v,d,x,j,t,e=(f(784,100),f(1,100),f(100,50),f(1,50),f(50,10),f(1,10),10000,0)
 for i in range(t*6):
-    q=p(xt[i],w)+c
-    n=p(q,v)+d
+    q=o(p(xt[i],w)+c)
+    n=o(p(q,v)+d)
     s=p(n,x)+j
     r=exp(s-max(s))/sum(exp(s-max(s)))
-    b(c,w,xt[i],b(d,v,q,b(j,x,n,(r-yt[i])/10)))
+    b(c,w,xt[i],b(d,v,q,b(j,x,n,r-yt[i])*k(n))*k(q))
     e-=log(r[0,argmax(yt[i])])
     if (i+1)%t==0:
         print(f"At {i+1}/{t*6} the error is {e/t}")

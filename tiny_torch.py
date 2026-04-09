946ec4a6f3fc8d8bd58d833211cfff8de90ec69a
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2026-04-09 21:06:42 +0200

    update torch nn to also use relu

--- tiny_torch.py
+++ tiny_torch.py
@@ -5,12 +5,15 @@ from load_mnist import to_torch, x_test, x_train, y_test, y_train
 x_test, x_train, y_test, y_train = to_torch(x_test, x_train, y_test, y_train)
 
 
-def mse(y_true, y_pred):
-    return to.mean((y_true - y_pred) ** 2)
+def cross_entropy(y_true, y_pred):
+    return -to.log(y_pred[0, to.argmax(y_true)])
 
 
-def mse_prime(y_true, y_pred):
-    return 2 * (y_pred - y_true) / to.numel(y_true)
+def test_error_rate(y_true, y_pred):
+    return to.argmax(y_true) != to.argmax(y_pred)
+
+relu = lambda x: to.clamp(x, min=0)
+relu_prime = lambda x: (x > 0)
 
 
 def softmax(x):
@@ -18,10 +21,10 @@ def softmax(x):
     return exp_values / to.sum(exp_values, dim=1, keepdim=True)
 
 
-def backprop_fc(bias, weights, input, output_error):
+def backprop_fc(bias: to.Tensor, weights: to.Tensor, input: to.Tensor, output_error: to.Tensor):
     input_error = to.matmul(output_error, weights.T)
-    weights -= 0.04 * to.matmul(input.T, output_error)
-    bias -= 0.04 * to.sum(output_error, dim=0, keepdim=True)
+    weights -= lr * to.matmul(input.T, output_error)
+    bias -= lr * to.sum(output_error, dim=0, keepdim=True)
     return input_error
 
 
@@ -31,18 +34,20 @@ def train():
         """forward propagation"""
         pixels = x_train[i]
 
-        output1 = to.matmul(pixels, w1) + b1
-        output2 = to.matmul(output1, w2) + b2
+        output1 = relu(to.matmul(pixels, w1) + b1)
+        output2 = relu(to.matmul(output1, w2) + b2)
         output3 = to.matmul(output2, w3) + b3
         prediction = softmax(output3)
 
         """backward propagation"""
-        error = mse_prime(y_train[i], prediction)
+        error = prediction - y_train[i]
         error = backprop_fc(b3, w3, output2, error)
+        error *= relu_prime(output2)
         error = backprop_fc(b2, w2, output1, error)
+        error *= relu_prime(output1)
         error = backprop_fc(b1, w1, pixels, error)
 
-        train_loss += mse(y_train[i], prediction)
+        train_loss += cross_entropy(y_train[i], prediction)
         if (i + 1) % 7500 == 0:
             print(f"At {i + 1}/{60000} the error is {train_loss / 7500:.3f}")
             train_loss = 0
@@ -54,14 +59,14 @@ def test():
         """forward propagation"""
         pixels = x_test[i]
 
-        output = to.matmul(pixels, w1) + b1
-        output = to.matmul(output, w2) + b2
-        output = to.matmul(output, w3) + b3
-        prediction = softmax(output)
+        output = relu(to.matmul(pixels, w1) + b1)
+        output = relu(to.matmul(output, w2) + b2)
+        prediction = to.matmul(output, w3) + b3
 
-        test_loss += mse(y_test[i], prediction)
+        test_loss += test_error_rate(y_test[i], prediction)
     print(f"Test loss: {test_loss / 10000:.3f}")
 
+lr = 0.02
 
 w1 = to.rand(28 * 28, 100) - 0.5
 b1 = to.rand(1, 100) - 0.5

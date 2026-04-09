8a081ca1c3af0fbc3d35fd3dee75d2bb47856512
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2026-04-09 16:21:24 +0200

    move error calculation before backprop for clarity

--- tiny_net.py
+++ tiny_net.py
@@ -35,16 +35,17 @@ def train():
         output3 = np.dot(output2, w3) + b3
         prediction = softmax(output3)
 
+        train_loss += mse(label, prediction)
+
+        if (i + 1) % 7500 == 0:
+            print(f"At {i + 1}/{60000} the error is {train_loss / 7500:.3f}")
+            train_loss = 0
+
         """backward propagation"""
         error = mse_prime(label, prediction)
         error = backprop_fc(b3, w3, output2, error)
         error = backprop_fc(b2, w2, output1, error)
         error = backprop_fc(b1, w1, pixels, error)
-
-        train_loss += mse(label, prediction)
-        if (i + 1) % 7500 == 0:
-            print(f"At {i + 1}/{60000} the error is {train_loss / 7500:.3f}")
-            train_loss = 0
 
 
 def test():

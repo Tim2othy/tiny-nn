df40ae8e8d768ece390a77a890f13a1dfe3e8472
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2026-04-09 20:01:40 +0200

    change relu prime order to increase clarity

--- nn_golf.py
+++ nn_golf.py
@@ -14,7 +14,7 @@ for i in range(t*6):
     n=R(D(q,v)+b)
     s=D(n,w)+c
     r=exp(s-max(s))/sum(exp(s-max(s)))
-    B(a,u,xt[i],B(b,v,q,B(c,w,n,r-yt[i])*(n>0))*(q>0))
+    B(a,u,xt[i],(q>0)*B(b,v,q,(n>0)*B(c,w,n,r-yt[i])))
     e-=log(r[0,argmax(yt[i])])
     if (i+1)%t==0:
         print(f"At {i+1}/{t*6} the error is {e/t}")

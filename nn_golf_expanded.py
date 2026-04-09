008782542f81028c4e7cd299d5b9c0add17b9de6
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2026-04-09 19:56:20 +0200

    use D for dot in golf expanded

--- nn_golf_expanded.py
+++ nn_golf_expanded.py
@@ -2,11 +2,11 @@ from numpy import dot, exp, max, mean, random, sum
 from load_mnist import xs, xt, ys, yt
 
 softmax = lambda x: exp(x - max(x)) / sum(exp(x - max(x)))
-
+D = dot
 
 def b(bias, weights, input, output_error):
-    g = dot(output_error, weights.T)
-    weights -= 0.04 * dot(input.T, output_error)
+    g = D(output_error, weights.T)
+    weights -= 0.04 * D(input.T, output_error)
     bias -= 0.04 * sum(output_error)
     return g
 
@@ -22,9 +22,9 @@ error = 0
 
 
 for i in range(60000):
-    q = dot(xt[i], w1) + b1
-    n = dot(q, w2) + b2
-    r = softmax(dot(n, w3) + b3)
+    q = D(xt[i], w1) + b1
+    n = D(q, w2) + b2
+    r = softmax(D(n, w3) + b3)
     b(b1, w1, xt[i], b(b2, w2, q, b(b3, w3, n, (r - yt[i]) / r.size)))
     error += mean((yt[i] - r) ** 2)
 
@@ -33,6 +33,6 @@ for i in range(60000):
         error = 0
 
 for i in range(10000):
-    error += mean((ys[i] - softmax(dot(dot(dot(xs[i], w1) + b1, w2) + b2, w3) + b3)) ** 2)
+    error += mean((ys[i] - softmax(D(D(D(xs[i], w1) + b1, w2) + b2, w3) + b3)) ** 2)
 
 print(f"Test loss: {error/10000}")

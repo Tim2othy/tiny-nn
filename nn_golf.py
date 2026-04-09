9914dccdec9c3e8b7be86423316c98d8eac4b7c8
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2026-04-09 19:06:41 +0200

    use correct test error loss function for golf

--- nn_golf.py
+++ nn_golf.py
@@ -19,5 +19,5 @@ for i in range(t*6):
         print(f"At {i+1}/{t*6} the error is {e/t}")
         e=0
 for i in range(t):
-    e+=mean((ys[i]-s(p(p(p(xs[i],w)+c,v)+d,x)+j))**2)
+    e+=argmax(ys[i])!=argmax(p(p(p(xs[i],w)+c,v)+d,x)+j)
 print(f"Test loss: {e/t}")

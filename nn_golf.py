9e4197677cf7d3653eeae0e438ea68ac32ad6bcb
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2026-04-09 20:47:14 +0200

    use better parenthesis

--- nn_golf.py
+++ nn_golf.py
@@ -10,7 +10,7 @@ def B(b,w,i,e):
 I=lambda x,y:random.rand(x,y)-.5
 u,v,w,a,b,c,t,e=(I(784,99),I(99,50),I(50,10),I(1,99),I(1,50),I(1,10),10000,0)
 for i in range(t*6):
- r=exp(m:=(s:=D((n:=R(D((q:=R(D(xt[i],u)+a)),v)+b)),w)+c)-max(s))/sum(exp(m))
+ r=exp((m:=(s:=D(n:=R(D(q:=R(D(xt[i],u)+a),v)+b),w)+c)-max(s)))/sum(exp(m))
  B(a,u,xt[i],(q>0)*B(b,v,q,(n>0)*B(c,w,n,r-yt[i])))
  e-=log(r[0,argmax(yt[i])])
  if-~i%t<1:

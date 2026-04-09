0ab83246c802816276a3384473ee05364ebba321
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2026-04-09 20:36:28 +0200

    more concise if clause

--- nn_golf.py
+++ nn_golf.py
@@ -16,7 +16,7 @@ for i in range(t*6):
  r=exp(s-max(s))/sum(exp(s-max(s)))
  B(a,u,xt[i],(q>0)*B(b,v,q,(n>0)*B(c,w,n,r-yt[i])))
  e-=log(r[0,argmax(yt[i])])
- if (i+1)%t==0:
+ if-~i%t<1:
   print(f"At {i+1}/{t*6} the error is {e/t}")
   e=0
 for i in range(t):
